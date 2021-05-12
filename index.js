eb1685521a2f5823d949a7430a7ed46884a5dc1d
jean <jin.ahn.1027@gmail.com>
2021-05-12 16:45:10 -0400

    commit tree beb0d9339f4637fede3b81cda95cde210c2d1fad

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+function map(arr, func){
+    let newarr=[];
+    for(let i=0; i<arr.length; i++){
+        newarr[i]=func(arr[i]);
+    }
+    return newarr;
+}
+
+function reduce(arr, func, start){
+    if (!start){
+        start=arr[0];
+    }
+    else{
+        start+=arr[0];
+    }
+    for(let i=1; i<arr.length; i++){
+        start=func(arr[i], start);
+    }
+    return start;
+}
